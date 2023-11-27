5d8e06ca561b7b0c6b024b028f8018a8992098ce
471867900@qq.com <471867900@qq.com>
2023-11-27 11:34:21 +0800

    electron 做一个代码生成器

--- template/electron-vite-vue-tailwindcss-element-plus/scripts/watch.mjs
+++ template/electron-vite-vue-tailwindcss-element-plus/scripts/watch.mjs
@@ -12,91 +12,92 @@ const debug = query.has('debug');
 
 /*日志输出处理 start*/
 function electronLog(data, color) {
-    if (data) {
-        let log = '';
-        data = data.toString().split(/\r?\n/);
-        data.forEach(line => {
-            log += `  ${line}\n`;
-        });
-        console.log(
-            chalk[color].bold('┏ Electron -------------------') +
-            '\n\n' +
-            log +
-            chalk[color].bold('┗ ----------------------------') +
-            '\n',
-        );
-    }
+  if (data) {
+    let log = '';
+    data = data.toString().split(/\r?\n/);
+    data.forEach(line => {
+      log += `  ${line}\n`;
+    });
+    console.log(
+      chalk[color].bold('┏ Electron -------------------') +
+      '\n\n' +
+      log +
+      chalk[color].bold('┗ ----------------------------') +
+      '\n',
+    );
+  }
 }
 
 /*日志输出处理 end*/
+
 // 演示如何在开发时使用自动复制文件到指定目录（实际目前是不需要这样做的）
 async function copyBin() {
-    // url.fileURLToPath()函数会根据当前操作系统平台自动处理文件路径。在macOS和其他类Unix系统上，它会正确地处理URL对象的pathname属性，而在Windows上，它会移除多余的斜杠
-    const basePath = fileURLToPath(new URL('../', import.meta.url));
-    const srcDir = path.normalize(path.join(basePath, 'bin'));
-    const destDir = path.normalize(path.join(basePath, 'dist', 'main', 'bin'));
-    console.log('srcDir', srcDir);
-    console.log('destDir', destDir);
-    fs.existsSync(destDir) && fs.removeSync(destDir);
-    await fs.copy(srcDir, destDir);
-    console.log('bin directory copied to dist/main/bin');
+  // url.fileURLToPath()函数会根据当前操作系统平台自动处理文件路径。在macOS和其他类Unix系统上，它会正确地处理URL对象的pathname属性，而在Windows上，它会移除多余的斜杠
+  const basePath = fileURLToPath(new URL('../', import.meta.url));
+  const srcDir = path.normalize(path.join(basePath, 'bin'));
+  const destDir = path.normalize(path.join(basePath, 'dist', 'main', 'bin'));
+  console.log('srcDir', srcDir);
+  console.log('destDir', destDir);
+  fs.existsSync(destDir) && fs.removeSync(destDir);
+  await fs.copy(srcDir, destDir);
+  console.log('bin directory copied to dist/main/bin');
 }
 
 
 function watchMain(server) {
-    /**
-     * @type {import('child_process').ChildProcessWithoutNullStreams | null}
-     */
-    let electronProcess = null;
-    const address = server.httpServer.address();
-    const env = Object.assign(process.env, {
-        VITE_DEV_SERVER_HOST: address.address,
-        VITE_DEV_SERVER_PORT: address.port,
-    });
-    /**
-     * @type {import('vite').Plugin}
-     */
-    const startElectron = {
-        name: 'electron-main-watcher',
-        writeBundle() {
-            electronProcess && electronProcess.kill();
-            electronProcess = spawn(electron, ['.'], {stdio: 'inherit', env});
+  /**
+   * @type {import('child_process').ChildProcessWithoutNullStreams | null}
+   */
+  let electronProcess = null;
+  const address = server.httpServer.address();
+  const env = Object.assign(process.env, {
+    VITE_DEV_SERVER_HOST: address.address,
+    VITE_DEV_SERVER_PORT: address.port,
+  });
+  /**
+   * @type {import('vite').Plugin}
+   */
+  const startElectron = {
+    name: 'electron-main-watcher',
+    writeBundle() {
+      electronProcess && electronProcess.kill();
+      electronProcess = spawn(electron, ['.'], {stdio: 'inherit', env});
 
-            electronLog('开始启动', 'blue');
+      electronLog('开始启动', 'blue');
 
-        },
-    };
+    },
+  };
 
-    return build({
-        configFile: 'packages/main/vite.config.ts',
-        mode: 'development',
-        plugins: [!debug && startElectron].filter(Boolean),
-        build: {
-            watch: {},
-        },
-    }).then(() => {
-        // Vite 构建结束后，将 bin 目录复制到 dist/main 目录下
-        // return copyBin();
-    });
+  return build({
+    configFile: 'packages/main/vite.config.ts',
+    mode: 'development',
+    plugins: [!debug && startElectron].filter(Boolean),
+    build: {
+      watch: {},
+    },
+  }).then(() => {
+    // Vite 构建结束后，将 bin 目录复制到 dist/main 目录下
+    // return copyBin();
+  });
 }
 
 /**
  * @type {(server: import('vite').ViteDevServer) => Promise<import('rollup').RollupWatcher>}
  */
 function watchPreload(server) {
-    return build({
-        configFile: 'packages/preload/vite.config.ts',
-        mode: 'development',
-        plugins: [{
-            name: 'electron-preload-watcher',
-            writeBundle() {
-                server.ws.send({type: 'full-reload'});
-            },
-        }],
-        build: {
-            watch: {},
-        },
-    });
+  return build({
+    configFile: 'packages/preload/vite.config.ts',
+    mode: 'development',
+    plugins: [{
+      name: 'electron-preload-watcher',
+      writeBundle() {
+        server.ws.send({type: 'full-reload'});
+      },
+    }],
+    build: {
+      watch: {},
+    },
+  });
 }
 
 // bootstrap
